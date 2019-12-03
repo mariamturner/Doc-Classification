b889bab394b024327162d9fe3e7111d09f26e07e
Maria Turner <maturner@seas.upenn.edu>
2019-12-03 04:43:11 -0500

    Update classify.py

--- classify.py
+++ classify.py
@@ -9,13 +9,12 @@ def get_vocabulary(D):
     document collection plus the "<unk>" token.
     """
     once = set()
-    tokens = set(["<unk>"])
+    tokens = set()
     for doc in D:
         for token in doc:
-            if token in once:
-                tokens.add(token)
-            else:
-                once.add(token)
+            if token in once: tokens.add(token)         
+            else: once.add(token)
+    tokens.add("<unk>")
     return tokens
 
 
@@ -29,10 +28,8 @@ class BBoWFeaturizer(object):
         """
         dict = {}
         for token in doc:
-            if token in vocab:
-                dict[token] = 1
-            else:
-                dict["<unk>"] = 1
+            if token in vocab: dict[token] = 1
+            else: dict["<unk>"] = 1
         return dict
 
 class CBoWFeaturizer(object):
@@ -46,15 +43,11 @@ class CBoWFeaturizer(object):
         dict = {}
         for token in doc:
             if token in vocab:
-                if token in dict:
-                    dict[token] += 1
-                else:
-                    dict[token] = 1
+                if token in dict: dict[token] += 1
+                else: dict[token] = 1
             else:
-                if "<unk>" in dict:
-                    dict["<unk>"] += 1
-                else:
-                    dict["<unk>"] = 1
+                if "<unk>" in dict: dict["<unk>"] += 1
+                else: dict["<unk>"] = 1
         return dict
 
 def compute_idf(D, vocab):
@@ -65,27 +58,21 @@ def compute_idf(D, vocab):
     maps from the token to the IDF value. If a token is not present in the
     vocab, it should be mapped to "<unk>".
     """
-    doc_set_size = len(D)
     docs_for_token = {}
     idfs = {}
 
     for i, doc in enumerate(D):
-        #doc_set_size += 1
         for token in doc:
             if token in vocab:
-                if token in docs_for_token:
-                    docs_for_token[token].add(i)
-                else:
-                    docs_for_token[token] = {i}
+                if token in docs_for_token: docs_for_token[token].add(i)
+                else: docs_for_token[token] = {i}
             else:
-                if "<unk>" in docs_for_token:
-                    docs_for_token["<unk>"].add(i)
-                else:
-                    docs_for_token["<unk>"] = {i}
+                if "<unk>" in docs_for_token: docs_for_token["<unk>"].add(i)
+                else: docs_for_token["<unk>"] = {i}
 
     for token in docs_for_token:
         # underflow
-        idfs[token] = numpy.log(doc_set_size / len(docs_for_token[token]))
+        idfs[token] = numpy.log(len(D) / len(docs_for_token[token]))
     keys = idfs.keys()
     for token in (vocab - set(keys)):
         idfs[token] = 0
@@ -129,3 +116,64 @@ def convert_to_features(D, featurizer, vocab):
     for doc in D:
         X.append(featurizer.convert_document_to_feature_dictionary(doc, vocab))
     return X
+
+
+def train_naive_bayes(X, y, k, vocab):
+    """
+    Computes the statistics for the Naive Bayes classifier.
+    X is a list of feature representations, where each representation
+    is a dictionary that maps from the feature name to the value.
+    y is a list of integers that represent the labels.
+    k is a float which is the smoothing parameters.
+    vocab is the set of vocabulary tokens.
+    
+    Returns two values:
+        p_y: A dictionary from the label to the corresponding p(y) score
+        p_v_y: A nested dictionary where the outer dictionary's key is
+            the label and the innner dictionary maps from a feature
+            to the probability p(v|y). For example, `p_v_y[1]["hello"]`
+            should be p(v="hello"|y=1).
+    """
+    
+    raise NotImplementedError
+
+
+def predict_naive_bayes(D, p_y, p_v_y):
+    """
+    Runs the prediction rule for Naive Bayes. D is a list of documents,
+    where each document is a list of tokens.
+    p_y and p_v_y are output from `train_naive_bayes`.
+    
+    Note that any token which is not in p_v_y should be mapped to
+    "<unk>". Further, the input dictionaries are probabilities. You
+    should convert them to log-probabilities while you compute
+    the Naive Bayes prediction rule to prevent underflow errors.
+    
+    Returns two values:
+        predictions: A list of integer labels, one for each document,
+            that is the predicted label for each instance.
+        confidences: A list of floats, one for each document, that is
+            p(y|d) for the corresponding label that is returned.
+    """
+    raise NotImplementedError
+
+
+def train_semi_supervised(X_sup, y_sup, D_unsup, X_unsup, D_valid, y_valid, k, vocab, mode):
+    """
+    Trains the Naive Bayes classifier using the semi-supervised algorithm.
+    
+    X_sup: A list of the featurized supervised documents.
+    y_sup: A list of the corresponding supervised labels.
+    D_unsup: The unsupervised documents.
+    X_unsup: The unsupervised document representations.
+    D_valid: The validation documents.
+    y_valid: The validation labels.
+    k: The smoothing parameter for Naive Bayes.
+    vocab: The vocabulary as a set of tokens.
+    mode: either "threshold" or "top-k", depending on which selection
+        algorithm should be used.
+    
+    Returns the final p_y and p_v_y (see `train_naive_bayes`) after the
+    algorithm terminates.    
+    """
+    raise NotImplementedError
